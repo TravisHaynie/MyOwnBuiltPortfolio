3cf84037b753a527600a4b1f5a11e636bf1c17f3
TravisHaynie <travihay@gmail.com>
2025-12-02 11:37:02 -0800

    C:added footer and resume options

--- My-Portfolio/src/components/Contact.jsx
+++ My-Portfolio/src/components/Contact.jsx
@@ -3,7 +3,10 @@ export default function Contact() {
         <div id="contact" className="contact-container">
           <h2 className="contact-name">Contact</h2>  
           <p className="contact-p1">If you'd like to reach out, feel free to Email me:</p>
-          <a href="mailto:travihay@gmail.com" className="email-link">travihay@gmail.com</a>
+          <div className="links-container">
+            <a href="mailto:travihay@gmail.com" className="contact-link">travihay@gmail.com</a>
+            <a href="" className="contact-link">Resume</a>
+          </div>
           <p className="contact-p2">Based in Portland,OR, but open to remote roles</p>
         </div>
     )
--- My-Portfolio/src/components/Footer.jsx
+++ My-Portfolio/src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import facebook from "../img/facebook.png"
+import github from "../img/github.png"
+import instagram from "../img/instagram.png"
+
 export default function Footer() {
     return(
-        <footer className="footer-background">
-            <img></img>
+        <footer className="footer-container">
+            <a href="https://github.com/TravisHaynie"><img className="width margin" src={github} alt="github image" /></a>
+            <a href="https://www.facebook.com/tchaynie"><img className="width " src={facebook} alt="facebook image" /></a>
+            <a href="https://www.instagram.com/travishaynie/"><img className="width margin" src={instagram} alt="instagram image" /></a>
         </footer>
     )
 }
