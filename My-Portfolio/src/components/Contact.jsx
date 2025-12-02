3b018d0f67dcd1d38015fa6c5770a3e7008ce1f3
TravisHaynie <travihay@gmail.com>
2025-12-02 15:14:23 -0800

    D:finilizing by adding lilinks to other projects

--- My-Portfolio/src/components/Contact.jsx
+++ My-Portfolio/src/components/Contact.jsx
@@ -4,8 +4,9 @@ export default function Contact() {
           <h2 className="contact-name">Contact</h2>  
           <p className="contact-p1">If you'd like to reach out, feel free to Email me:</p>
           <div className="links-container">
-            <a href="mailto:travihay@gmail.com" className="contact-link">travihay@gmail.com</a>
-            <a href="" className="contact-link">Resume</a>
+            <a href="mailto:travihay@gmail.com" className="contact-link" target="_blank">travihay@gmail.com</a>
+            <a href="./public/Temp-Cloud_resume.pdf" className="contact-link" target="_blank">Cloud Resume</a>
+            <a href="./public/Front-End-Resume.pdf" className="contact-link" target="_blank">Front-End Resume</a>
           </div>
           <p className="contact-p2">Based in Portland,OR, but open to remote roles</p>
         </div>
