fe2cb6311d6b5277165bc21d64b1da88303dccf3
TravisHaynie <travihay@gmail.com>
2025-12-19 13:50:22 -0800

    added aws project

--- My-Portfolio/src/components/Projects.jsx
+++ My-Portfolio/src/components/Projects.jsx
@@ -10,19 +10,19 @@ export default function Projects() {
         <div className="projects-container">
           <div className="project-card">
             <h2 className="project-titles">Movie Watchlist</h2>
-            <a href="https://moviewatchlistscrimba.netlify.app/" target="_blank"><img src={MovieWatchlistImg} className="project-img"alt="movie-watchlist" /></a>
+            <a href="https://my-portfolio-travis.netlify.app/" target="_blank"><img src={MovieWatchlistImg} className="project-img"alt="movie-watchlist" /></a>
           </div>
            <div className="project-card">
             <h2 className="project-titles">My React Portfolio</h2>
-            <a href="https://my-porfolio-travis.netlify.app/" target="_blank"><img src={MyPortfolio} className="project-img"alt="movie-watchlist" /></a>
+            <a href="https://my-portfolio-travis.netlify.app/" target="_blank"><img src={MyPortfolio} className="project-img"alt="movie-watchlist" /></a>
           </div>
            <div className="project-card">
             <h2 className="project-titles">Color Scheme Generator</h2>
             <a href="https://dazzling-choux-44e22b.netlify.app/" target="_blank"><img src={GetCOlorSchemes} className="project-img"alt="movie-watchlist" /></a>
           </div>
            <div className="project-card">
-            <h2 className="project-titles">Aws Project(soon to be released)</h2>
-            <a href="https://moviewatchlistscrimba.netlify.app/" target="_blank"><img src={MovieWatchlistImg} className="project-img"alt="movie-watchlist" /></a>
+            <h2 className="project-titles">Aws Project</h2>
+            <a href="https://d2qi4m4eaq4ssx.cloudfront.net/" target="_blank"><img src={MyPortfolio} className="project-img"alt="movie-watchlist" /></a>
           </div>
         </div>
       </div>
